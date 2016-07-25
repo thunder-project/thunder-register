b4932c72887dba2a1fc5e8c15ef5144142b796f9
d-v-b <vaslav.tchitcherine@gmail.com>
2016-07-25 10:39:20 -0400

    fixed some tab/space issues

--- test/test_crosscorr.py
+++ test/test_crosscorr.py
@@ -6,31 +6,32 @@ from registration import CrossCorr
 
 pytestmark = pytest.mark.usefixtures("eng")
 
+
 def test_fit(eng):
     reference = arange(25).reshape(5, 5)
-	algorithm = CrossCorr()
-	deltas = [[1, 2], [-2, 1]]
-	shifted = [shift(reference, delta, mode='wrap', order=0) for delta in deltas]
-	model = algorithm.fit(shifted, reference=reference)
-	assert allclose(model.toarray(), deltas)
+    algorithm = CrossCorr()
+    deltas = [[1, 2], [-2, 1]]
+    shifted = [shift(reference, delta, mode='wrap', order=0) for delta in deltas]
+    model = algorithm.fit(shifted, reference=reference)
+    assert allclose(model.toarray(), deltas)
 
 
 def test_fit_3d(eng):
-	reference = arange(125).reshape(5, 5, 5)
-	algorithm = CrossCorr()
-	deltas = [[1, 0, 2], [0, 1, 2]]
-	shifted = [shift(reference, delta, mode='wrap', order=0) for delta in deltas]
-	model = algorithm.fit(shifted, reference=reference)
-	assert allclose(model.toarray(), deltas)
+    reference = arange(125).reshape(5, 5, 5)
+    algorithm = CrossCorr()
+    deltas = [[1, 0, 2], [0, 1, 2]]
+    shifted = [shift(reference, delta, mode='wrap', order=0) for delta in deltas]
+    model = algorithm.fit(shifted, reference=reference)
+    assert allclose(model.toarray(), deltas)
 
 
 def test_fit_axis(eng):
-	reference = arange(60).reshape(2, 5, 6)
-	algorithm = CrossCorr(axis=0)
-	a = shift(reference[0], [1, 2], mode='wrap', order=0)
-	b = shift(reference[1], [-2, 1], mode='wrap', order=0)
-	c = shift(reference[0], [2, 1], mode='wrap', order=0)
-	d = shift(reference[1], [1, -2], mode='wrap', order=0)
-	shifted = [asarray([a, b]), asarray([c, d]),]
-	model = algorithm.fit(shifted, reference=reference)
-	assert allclose(model.toarray(), [[[1, 2], [-2, 1]], [[2, 1], [1, -2]]])
+    reference = arange(60).reshape(2, 5, 6)
+    algorithm = CrossCorr(axis=0)
+    a = shift(reference[0], [1, 2], mode='wrap', order=0)
+    b = shift(reference[1], [-2, 1], mode='wrap', order=0)
+    c = shift(reference[0], [2, 1], mode='wrap', order=0)
+    d = shift(reference[1], [1, -2], mode='wrap', order=0)
+    shifted = [asarray([a, b]), asarray([c, d]),]
+    model = algorithm.fit(shifted, reference=reference)
+    assert allclose(model.toarray(), [[[1, 2], [-2, 1]], [[2, 1], [1, -2]]])
